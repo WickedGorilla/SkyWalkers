0438ca7933127c9ee700bc9b2e59f531ef5826e2
wickedgorilla <wickedgorilla@outlook.com>
2024-11-24 01:12:34 +0400

    Update domain

--- Assets/Scripts/Infrastructure/Network/Core/ServerRequestSender.cs
+++ Assets/Scripts/Infrastructure/Network/Core/ServerRequestSender.cs
@@ -11,7 +11,7 @@ namespace Infrastructure.Network
 {
     public class ServerRequestSender : IServerRequestSender
     {
-        private const string BaseUrl = "https://app.skywalkersgame.com";
+        private const string BaseUrl = "https://app.skywalkersgame.com/";
 
         private long _userId;
         private string _token;
